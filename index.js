9de6c59b022306a143dffc48c6dab75fba2eeea3
harshitbatra9 <hb5075@srmist.edu.in>
2023-02-13 19:32:29 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const btnel=document.getElementById("btn")
 const bmiInputel=document.getElementById("bmi-result")
 const weightcondel=document.getElementById("weight condition")
+const idealweightel=document.getElementById("ideal weight")
+const idealweightel2=document.getElementById("ideal weight2")
 function calculateBMI(){
     const heightValue=document.getElementById("height").value/100
     const weightValue=document.getElementById("weight").value 
@@ -11,15 +13,21 @@ function calculateBMI(){
         weightcondel.innerText="Under Weight"
 
     }
-    else if(bmiValue>=18.5 && bmiValue<24.9){
+    else if(bmiValue>=18.500 && bmiValue<24.9999){
         weightcondel.innerText="Normal Weight"
     }
-    else if (bmiValue>=25 && bmiValue<=29.9){
+    else if (bmiValue>=25 && bmiValue<=29.99999){
         weightcondel.innerText="Overweight"
     }
     else if(bmiValue>=30) {
         weightcondel.innerText="Obesity"
     }
+    const y=18.500*heightValue*heightValue;
+    const l=Math.round(y * 10) / 10
+    const z=24.999*heightValue*heightValue;
+    idealweightel.innerText=l;
+    const ll=Math.round(z * 10) / 10
+    idealweightel2.innerText=ll;
 }
 
 
